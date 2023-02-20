5a037d8153d55c48d32603b63a2d7f3d96f7dc35
andrewstewart1993@gmail.com <andystewart@Andrews-MacBook-Air.local>
2023-02-20 12:53:39 +0000

    pseudo code

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+// contast variables which will not be changed throughout the code.
+const inquirer = require('inquirer');
+const fs = require ('fs');
 
+//prompts for my readme
+
+//Title of my project
+
+
+//Sections
+
+  //Description
+
+  //Table of contents
+
+  //Installation
+
+  //Usage
+
+  //license
+
+  //Contributing
+
+  //Tests
+
+  //Questions
