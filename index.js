63ff03736dfa71bb1eb9edd38c6fdab333003c63
andrewstewart1993@gmail.com <andystewart@Andrews-MacBook-Air.local>
2023-02-20 20:23:29 +0000

    more chnages to the starter code

--- index.js
+++ index.js
@@ -1,26 +1,65 @@
-// contast variables which will not be changed throughout the code.
-const inquirer = require('inquirer');
-const fs = require ('fs');
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-//prompts for my readme
+const questions = [
+    //Title of my project - this should be input
+    {
+        type: 'input',
+        message: 'What is the title of this project?',
+        name: 'titleSelect',
+    },
+    //Sections
 
-//Title of my project
+    //Description - this should be input
+    {
+        type: 'input',
+        message: 'Write a description for your project',
+        name: 'descSelect',
+    },
+    //Table of contents - research this 
 
+    //Installation
+    {
+        type: 'input',
+        message: 'How do you install this package?',
+        name: 'instSelect',
+    },
+    //Usage
+    {
+        type: 'input',
+        message: 'How do you use this package?',
+        name: 'useSelect',
+    },
+    //license - checkbox
+    {
+        type: 'list',
+        message: 'What liscense does this use?',
+        name: 'liscSelect',
+        choices: ['GNU AGPLv3', 'GNU GPLv3',
+            'GNU LGPLv3', 'Mozilla Public License 2.0',
+            'Apache License 2.0', 'MIT License', 'Boost Software License 1.0',
+            'The Unlicense'],
+    }
 
-//Sections
+        Contributing
 
-  //Description
+        Tests
 
-  //Table of contents
+        Questions
+])
+];
 
-  //Installation
+// function to write README file
+function writeToFile(fileName, data) {
+}
 
-  //Usage
+// function to initialize program
+function init() {
 
-  //license
+}
 
-  //Contributing
+// function call to initialize program
+init();
 
-  //Tests
-
-  //Questions
